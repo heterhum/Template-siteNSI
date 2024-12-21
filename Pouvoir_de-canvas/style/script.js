d39ad8b7f07d4ce06b45712134ae64cfbf5ff5cf
heterhum <160785637+heterhum@users.noreply.github.com>
2024-12-21 14:24:01 +0100

    jdlv essaie BUG

--- Pouvoir_de-canvas/style/script.js
+++ Pouvoir_de-canvas/style/script.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded',domloaded,false);
+//$(document).ready( function(){
 
+//}
 function domloaded(){
     window.c = document.getElementById("myCanvas");
     window.ctx = c.getContext("2d");
-    const canvasWidth = c.offsetWidth;
-    const canvasHeight = c.offsetHeight;
-
-// Ajuster les dimensions internes du canvas
-    c.width = canvasWidth;
-    c.height = canvasHeight;
-    
+    window.largeur=50;
+    window.longueur=50;
+    c.width = c.offsetWidth;
+    c.height = c.offsetHeight;
+    console.log(c.width,c.height)
+    window.taille=c.width/largeur;
 }
 function doit(){
     ctx.moveTo(0, 0);
@@ -28,19 +29,28 @@ function getMousePos(canvas, evt) {
         y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
     };
   }
-var taillex=10
-var tailley=10
+
 function draw(e){
     let a=[]
-    for (let i = 0; i < tailley; i++) {
+    for (let i = 0; i < largeur; i++) {
         let b=[]
-        for (let i = 0; i < tailley; i++) {
-            b[i]=0
+        for (let j = 0; j < longueur; j++) {
+            b[j]=0
         }
         a[i]=b
     }
-    ctx.fillStyle=10;
-    ctx.fillRect(200,200,20,20)
+    for (let i=0;i<a.length; i++){
+        for (let j=0;j<a.length; j++){
+            if (a[i][j]==0){ 
+                ctx.fillStyle="pink";
+                ctx.fillRect(j*taille,i*taille,taille,taille)
+            }  else{
+                ctx.fillStyle="black";
+                ctx.fillRect(j*taille,i*taille,taille,taille)
+            }
+            ctx.strokeRect(j*taille, i*taille, taille, taille)
+        }
+    }
     //var pos=getMousePos(c,e);
     //console.log(pos.x,pos.y)
     //ctx.arc(pos.x, pos.y, 2, 0, 2 * Math.PI);
